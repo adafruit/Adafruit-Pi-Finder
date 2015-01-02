c5c1017155cf6ed7570249bd18fb29b2a6b7ce00
Brennen Bearnes <bbearnes@gmail.com>
2015-01-02 22:16:40 +0000

    use mktemp(1) instead of hardcoded /tmp/packages
    Not strictly necessary, but always a touch safer.

--- repo/deploy.sh
+++ repo/deploy.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-# make sure user passed a path the the repo
+# make sure user passed a path to the repo
 if [ "$1" == "" ]; then
   echo "You must specify a path to your pi_bootstrap repo. i.e. /home/admin/pi_bootstrap"
   exit 1
 fi
 
-# copy the packages to the temp folder
-mkdir /tmp/packages
-cp $1/packages/build/*.deb /tmp/packages/
+# copy the packages to a temp folder
+TEMP_DIR=`mktemp -d`
+cp $1/packages/build/*.deb $TEMP_DIR
 
 # sign packages, and add them to the repo
-dpkg-sig -k $GPG_KEY --sign builder /tmp/packages/*.deb
+dpkg-sig -k $GPG_KEY --sign builder $TEMP_DIR/*.deb
 cd /var/packages/raspbian/
-reprepro includedeb wheezy /tmp/packages/*.deb 2>/dev/null
+reprepro includedeb wheezy $TEMP_DIR/*.deb 2>/dev/null
 
 # clean up
-rm -r /tmp/packages
+rm -r $TEMP_DIR
