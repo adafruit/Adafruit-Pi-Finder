4b458a252c463bf2e4d3ff58a97720c94c20bf58
Todd Treece <toddtreece@gmail.com>
2015-01-02 22:05:11 +0000

    small tweaks to deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # make sure user passed a path the the repo
 if [ "$1" == "" ]; then
-  echo "You must specify a path to your pi_bootstrap repo. i.e. /home/todd/pi_bootstrap"
+  echo "You must specify a path to your pi_bootstrap repo. i.e. /home/admin/pi_bootstrap"
   exit 1
 fi
 
@@ -13,7 +13,7 @@ cp $1/packages/build/*.deb /tmp/packages/
 # sign packages, and add them to the repo
 dpkg-sig -k $GPG_KEY --sign builder /tmp/packages/*.deb
 cd /var/packages/raspbian/
-reprepro includedeb wheezy /tmp/packages/*.deb
+reprepro includedeb wheezy /tmp/packages/*.deb 2>/dev/null
 
 # clean up
 rm -r /tmp/packages
