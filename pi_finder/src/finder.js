f8e19c270421ff109d500969789bda3ee3dbd5e7
Todd Treece <toddtreece@gmail.com>
2015-01-07 08:03:30 -0500

    finished finder module

--- pi_finder/src/finder.js
+++ pi_finder/src/finder.js
@@ -1,7 +1,8 @@
 var ip = require('ip'),
     util = require('util'),
     ping = require('ping'),
-    async = require('async');
+    async = require('async'),
+    arp = require('arp-a');
 
 /**** Finder prototype ****/
 var proto = Finder.prototype;
@@ -27,14 +28,14 @@ function Finder(options) {
 
 proto.ip = false;
 proto.subnet = false;
-proto.count = 1;
+proto.position = 1;
 proto.pi = false;
 
 proto.start = function(cb) {
 
   async.doUntil(
     this.ping.bind(this),
-    this.arp.bind(this),
+    this.check.bind(this),
     this.finish.bind(this, cb)
   );
 
@@ -42,24 +43,51 @@ proto.start = function(cb) {
 
 proto.ping = function(next) {
 
-  var host = this.subnet + '.' + this.count;
+  var host = this.subnet + '.' + this.position;
 
   ping.sys.probe(host, function(){
 
-    this.count++;
+    this.position++;
 
-    if(this.count >= 254) {
-      this.count = 1;
+    if(this.position >= 254) {
+      this.position = 1;
     }
 
-    next();
+    this.arp(next);
 
   }.bind(this));
 
 };
 
-proto.arp = function() {
+proto.arp = function(next) {
+
+  arp.table(function(err, entry) {
+
+    if (err) {
+      return next(err);
+    }
+
+    if (!entry) {
+      return next();
+    }
+
+    if(/b8:27:eb/.test(entry.mac)) {
+      this.pi = entry.ip;
+      next();
+    }
+
+  }.bind(this));
 
 };
 
-proto.finish = function(cb)
+proto.check = function() {
+
+  return this.pi ? true : false;
+
+};
+
+proto.finish = function(cb, err) {
+
+  cb(err, this.pi);
+
+};
