3a22d50f9938b12f7d4ba55799bf2ea985e38f9f
Todd Treece <toddtreece@gmail.com>
2015-01-23 23:51:18 +0000

    bump webide version to 0.3.9

--- repo/deploy.sh
+++ repo/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 NODE_DEB='node_0.10.35_armhf.deb'
-IDE_DEB='adafruitwebide-0.3.8-Linux.deb'
+IDE_DEB='adafruitwebide-0.3.9-Linux.deb'
 
 # make sure user passed a path to the repo
 if [ "$1" == "" ]; then
