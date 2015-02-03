54c68a5e1184d11583f90d0d5e34ecdbd0fc3ae3
Todd Treece <toddtreece@gmail.com>
2015-02-03 13:17:58 -0500

    finder: only show user a messsage if auto update fails
    the install happens so quickly that it could be confusing
    to see the install message flash before the finder window
    appears.

--- pi_finder/main/main.js
+++ pi_finder/main/main.js
@@ -1,9 +1,6 @@
-var BrowserWindow = require('browser-window'),
-    app = require('app'),
-    ipc = require('ipc'),
+var app = require('app'),
     dialog = require('dialog'),
-    npm = require('npm'),
-    main;
+    npm = require('npm');
 
 app.on('window-all-closed', function() {
   app.quit();
@@ -11,30 +8,15 @@ app.on('window-all-closed', function() {
 
 app.on('ready', function() {
 
-  main = new BrowserWindow({
-    width: 300,
-    height: 300,
-    resizable: false,
-    'use-content-size': true
-  });
-
-  main.loadUrl('file://' + __dirname + '/ui/main.html');
-
-  main.on('closed', function() {
-    main = null;
-  });
-
   npm.load(function() {
 
     npm.commands.install(__dirname, ['adafruit-pi-finder@latest'], function(err) {
 
       if(err) {
-        return dialog.showErrorBox('ERROR', 'Update failed!');
+        return dialog.showErrorBox('ERROR', 'Pi Finder auto update failed! Are you connected to the internet?');
       }
 
       require('adafruit-pi-finder')(app);
-
-      main.close();
 
     });
 
