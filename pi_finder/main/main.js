a91f6aefa3faf38eebe12fc4c2ee70f2fe8724f3
Todd Treece <toddtreece@gmail.com>
2015-04-05 08:32:42 -0400

    only fail to start if pi finder package doesn't exist

--- pi_finder/main/main.js
+++ pi_finder/main/main.js
@@ -12,11 +12,11 @@ app.on('ready', function() {
 
     npm.commands.install(__dirname, ['adafruit-pi-finder@stable'], function(err) {
 
-      if(err) {
+      try {
+        require('adafruit-pi-finder')(app);
+      } catch(e) {
         return dialog.showErrorBox('ERROR', 'Pi Finder auto update failed! Are you connected to the internet?');
       }
-
-      require('adafruit-pi-finder')(app);
 
     });
 
