227e8856d6fe98f8388e9d9b80c859e739b19e74
Todd Treece <toddtreece@gmail.com>
2015-05-07 20:53:47 -0400

    switch all atom-shell references to electron in gruntfile

--- wrapper/Gruntfile.js
+++ wrapper/Gruntfile.js
@@ -1,10 +1,10 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    'build-atom-shell-app': {
+    'build-electron-app': {
       options: {
         build_dir: '../build',
         platforms: ['darwin', 'win32', 'linux32', 'linux64'],
-        cache_dir: (process.env.TMPDIR || process.env.TEMP || '/tmp') + 'atom-shell-cache',
+        cache_dir: (process.env.TMPDIR || process.env.TEMP || '/tmp') + 'electron-cache',
         app_dir: './'
       }
     },
@@ -13,50 +13,50 @@ module.exports = function(grunt) {
         force: true
       },
       all: ['../build', '*.zip', '*.tar', '*.tar.gz'],
-      symlink: ['../build/darwin/atom-shell/Atom.app/Contents/Resources/app']
+      symlink: ['../build/darwin/Electron.app/Contents/Resources/app']
     },
     rename: {
       mac: {
         files: [
-          { src: ['../build/darwin/atom-shell/Atom.app'], dest: '../build/darwin/atom-shell/Pi Finder.app' },
-          { src: ['../build/darwin/atom-shell/Pi Bootstrap.app/Contents/MacOS/Atom'], dest: '../build/darwin/atom-shell/Pi Bootstrap.app/Contents/MacOS/Pi Finder' }
+          { src: ['../build/darwin/Electron.app'], dest: '../build/darwin/Pi Finder.app' },
+          { src: ['../build/darwin/Pi Finder.app/Contents/MacOS/Electron'], dest: '../build/darwin/Pi Finder.app/Contents/MacOS/Pi Finder' }
         ]
       },
       linux: {
         files: [
-          { src: ['../build/linux32/atom-shell/atom'], dest: '../build/linux32/atom-shell/pifinder' },
-          { src: ['../build/linux64/atom-shell/atom'], dest: '../build/linux64/atom-shell/pifinder' }
+          { src: ['../build/linux32/electron'], dest: '../build/linux32/pifinder' },
+          { src: ['../build/linux64/electron'], dest: '../build/linux64/pifinder' }
         ]
       },
       win: {
         files: [
-          { src: ['../build/win32/atom-shell/atom.exe'], dest: '../build/win32/atom-shell/PiFinder.exe' }
+          { src: ['../build/win32/Electron.exe'], dest: '../build/win32/PiFinder.exe' }
         ]
       }
     },
     copy: {
       mac: {
         files: [
-          { src: 'icons/adafruit.icns', dest: '../build/darwin/atom-shell/Pi Finder.app/Contents/Resources/atom.icns' }
+          { src: 'icons/adafruit.icns', dest: '../build/darwin/Pi Finder.app/Contents/Resources/atom.icns' }
         ]
       },
       win: {
         files: [
-          { src: 'icons/adafruit.ico', dest: '../build/win32/atom-shell/resources/adafruit.ico' }
+          { src: 'icons/adafruit.ico', dest: '../build/win32/resources/adafruit.ico' }
         ]
       }
     },
     sed: {
       bundle: {
-        pattern: '<string>Atom</string>',
+        pattern: '<string>Electron</string>',
         replacement: '<string>Pi Finder</string>',
-        path: '../build/darwin/atom-shell/Pi Finder.app/Contents/Info.plist'
+        path: '../build/darwin/Pi Finder.app/Contents/Info.plist'
       }
     },
     winresourcer: {
       exe: {
         operation: 'Update',
-        exeFile: '../build/win32/atom-shell/PiFinder.exe',
+        exeFile: '../build/win32/PiFinder.exe',
         resourceType: 'Icongroup',
         resourceName: '1',
         lang: 1033,
@@ -69,8 +69,8 @@ module.exports = function(grunt) {
       },
       linux: {
         src: [
-          '../build/linux64/atom-shell/pifinder',
-          '../build/linux32/atom-shell/pifinder'
+          '../build/linux64/pifinder',
+          '../build/linux32/pifinder'
         ]
       }
     },
@@ -80,7 +80,7 @@ module.exports = function(grunt) {
       },
       dev: {
         src: './',
-        dest: '../build/darwin/atom-shell/Atom.app/Contents/Resources/app'
+        dest: '../build/darwin/Electron.app/Contents/Resources/app'
       }
     },
     compress: {
@@ -89,19 +89,19 @@ module.exports = function(grunt) {
       },
       mac: {
         options: {
-          archive: 'pifinder_mac.zip'
+          archive: '../build/pifinder_mac.zip'
         },
         expand: true,
-        cwd: '../build/darwin/atom-shell/',
+        cwd: '../build/darwin/',
         src: ['**'],
         dest: './'
       },
       windows: {
         options: {
-          archive: 'pifinder_windows.zip'
+          archive: '../build/pifinder_windows.zip'
         },
         expand: true,
-        cwd: '../build/win32/atom-shell/',
+        cwd: '../build/win32/',
         src: ['**'],
         dest:'./'
       },
@@ -111,7 +111,7 @@ module.exports = function(grunt) {
           mode: 'tgz'
         },
         expand: true,
-        cwd: '../build/linux32/atom-shell/',
+        cwd: '../build/linux32/',
         src: ['**'],
         dest:'pifinder/'
       },
@@ -121,7 +121,7 @@ module.exports = function(grunt) {
           mode: 'tgz'
         },
         expand: true,
-        cwd: '../build/linux64/atom-shell/',
+        cwd: '../build/linux64/',
         src: ['**'],
         dest:'pifinder/'
       }
@@ -129,7 +129,7 @@ module.exports = function(grunt) {
 
   });
 
-  grunt.loadNpmTasks('grunt-atom-shell-app-builder');
+  grunt.loadNpmTasks('grunt-electron-app-builder');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-compress');
   grunt.loadNpmTasks('grunt-contrib-rename');
@@ -139,7 +139,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-chmod');
   grunt.loadNpmTasks('grunt-contrib-symlink');
 
-  grunt.registerTask('default', ['clean:all', 'build-atom-shell-app', 'clean:symlink', 'symlink']);
-  grunt.registerTask('build', ['clean:all', 'build-atom-shell-app', 'rename', 'copy', 'chmod', 'sed', 'winresourcer', 'compress']);
+  grunt.registerTask('default', ['clean:all', 'build-electron-app', 'clean:symlink', 'symlink']);
+  grunt.registerTask('build', ['clean:all', 'build-electron-app', 'rename', 'copy', 'chmod', 'sed', 'winresourcer', 'compress']);
 
 };
